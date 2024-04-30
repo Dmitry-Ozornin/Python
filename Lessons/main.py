321fe5eb7e8ce6f915ba62bcfea709e932195ba9
Dmitry-Ozornin <158093210+Dmitry-Ozornin@users.noreply.github.com>
2024-04-30 18:02:19 +0300

    Add files via upload

---
+++ Lessons/main.py
@@ -0,0 +1,37 @@
+# n = 5
+
+
+# print(n)
+
+
+# n = 'fddf'
+# print(n)
+# n1 = "vvvv"
+# print(n1)
+
+# n = 'fd\'df'
+# print(n)
+
+
+
+# Интреполяция
+# a = 5
+# b = 5.89
+# c= 'Hello'
+
+# print(a, ' - ', b, ' - ', c)
+# print (f"{a} - {b} - {c}")
+# print("{} - {} - {}".format(a,b,c))
+
+# ввод данных
+# print('введите первою строку ')
+# a = input()
+
+# b = input('введите второе число ')
+
+# # print(a, ' + ', b, ' = ', a + b) делает строку(не складывает)
+
+c = 5.89
+print(type(c))
+n = int(c)
+print(n)
